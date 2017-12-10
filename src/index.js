642cac27ba2b385b91d0a669f5b44afde6e8ebb4
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-12-10 23:00:43 +0700

    Add initialState

--- src/index.js
+++ src/index.js
@@ -6,8 +6,12 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import thunk from 'redux-thunk'
+import initialState from './initialState'
+import {getAllList} from './actions/index'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(reducer, initialState, applyMiddleware(thunk))
+
+store.dispatch(getAllList())
 
 render(
     <Provider store={store}>
