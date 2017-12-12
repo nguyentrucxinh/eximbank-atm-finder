dd58a2c948298f56c4e54e50f1ae211e1cb68d99
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-12-12 23:35:07 +0700

    Add redux-logger, store subscribe

--- src/store.js
+++ src/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import reducer from './reducers'
 import initialState from './initialState'
 import { getAllList } from './actions/index'
 
-const store = createStore(reducer, initialState, applyMiddleware(thunk))
+const store = createStore(reducer, initialState, applyMiddleware(thunk), applyMiddleware(logger))
+
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
 store.dispatch(getAllList())
 
