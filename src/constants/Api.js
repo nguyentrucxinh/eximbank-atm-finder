9766eeef38e3721bb3b07a8d0fa84cb802a4cf34
Xinh Nguyen <nguyentrucxjnh@gmail.com>
2017-12-12 22:47:10 +0700

    Add city data

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,2 +1,3 @@
 export const GET_ALL_LIST = 'http://webapi.diadiem.com.vn/API.aspx'
 export const GET_LOCATIONS = 'http://webapi.diadiem.com/callService.aspx'
+export const GET_CITIES = 'http://webapi.diadiem.com/City.aspx'
